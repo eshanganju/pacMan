3e45e2044a080ce3ed9b94d446dba1dbe7b0c30e
eshanganju <ganju.eshan@gmail.com>
2019-07-30 09:43:12 -0400

    The main file - other classes will be added soon

---
+++ pac.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+'''
+PAC - Particle Analysis Code
+
+Analyze static CT scans for particle properties
+
+Features: 
+    1. Filter tiff stack
+    2. Binarize tiff stack
+    3. Segment tiff stack
+    4. Determine particle size parameters
+    5. Determine particle morphology parameters
+    6. Determine particle contacts
+    7. Determine particle orientation
+    8. Compute fabric tensor
+
+EG 2019
+'''
+
