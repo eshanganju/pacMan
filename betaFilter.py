d5ae67d6eff52508761d105225a1b3673f83488f
eshanganju <ganju.eshan@gmail.com>
2019-08-18 16:18:19 -0400

    NLM filter code updated in betaFilter.py

--- betaFilter.py
+++ betaFilter.py
@@ -8,7 +8,7 @@ import spam.kalisphera as skali
 import math
 import scipy
 
-f = open("logFileAlphaFilter.txt","w+")
+f = open("logFileBetaFilter.txt","w+")
 
 pixelSize = 30.e-6
 blurSTD = 0.8    
@@ -64,32 +64,9 @@ nlm_7ps_11pd_02h = restoration.denoise_nl_means(ori50,
                                                 fast_mode=True, 
                                                 sigma=sigma_est)
 
-ax1=plt.subplot(131)
-ax1.imshow(nlm_3ps_11pd_02h, cmap='Greys_r')
-plt.title('NLM3ps11pd0.02h')
-
-ax2=plt.subplot(132)
-ax2.imshow(nlm_5ps_11pd_02h, cmap='Greys_r')
-plt.title('NLM5ps11pd0.02h')
-
-ax3=plt.subplot(133)
-ax3.imshow(nlm_7ps_11pd_02h, cmap='Greys_r')
-plt.title('NLM7ps11pd0.02h')
-
-plt.savefig('plts_nlm.tiff',dpi=600) 
-plt.show()
-
 nlm_5ps_09pd_02h = restoration.denoise_nl_means(ori50, 
                                                 patch_size=5, 
                                                 patch_distance=9, 
-                                                h=0.02, 
-                                                multichannel=False, 
-                                                fast_mode=True, 
-                                                sigma=sigma_est)
-
-nlm_5ps_11pd_02h = restoration.denoise_nl_means(ori50, 
-                                                patch_size=5, 
-                                                patch_distance=11, 
                                                 h=0.02, 
                                                 multichannel=False, 
                                                 fast_mode=True, 
@@ -103,18 +80,88 @@ nlm_5ps_13pd_02h = restoration.denoise_nl_means(ori50,
                                                 fast_mode=True, 
                                                 sigma=sigma_est)
 
-ax1=plt.subplot(131)
-ax1.imshow(nlm_5ps_09pd_02h, cmap='Greys_r')
-plt.title('NLM5ps09pd0.02h')
+nlm_5ps_11pd_05h = restoration.denoise_nl_means(ori50, 
+                                                patch_size=5, 
+                                                patch_distance=11, 
+                                                h=0.05, 
+                                                multichannel=False, 
+                                                fast_mode=True, 
+                                                sigma=sigma_est)
 
-ax2=plt.subplot(132)
+nlm_5ps_11pd_10h = restoration.denoise_nl_means(ori50, 
+                                                patch_size=5, 
+                                                patch_distance=11,
+                                                h=0.1, 
+                                                multichannel=False, 
+                                                fast_mode=True, 
+                                                sigma=sigma_est)
+
+ax1=plt.subplot(331)
+ax1.imshow(nlm_3ps_11pd_02h, cmap='Greys_r')
+plt.title('NLM3ps11pd0.02h')
+
+ax2=plt.subplot(332)
 ax2.imshow(nlm_5ps_11pd_02h, cmap='Greys_r')
 plt.title('NLM5ps11pd0.02h')
 
-ax3=plt.subplot(133)
-ax3.imshow(nlm_5ps_13pd_02h, cmap='Greys_r')
+ax3=plt.subplot(333)
+ax3.imshow(nlm_7ps_11pd_02h, cmap='Greys_r')
+plt.title('NLM7ps11pd0.02h')
+
+ax4=plt.subplot(334)
+ax4.imshow(nlm_5ps_09pd_02h, cmap='Greys_r')
+plt.title('NLM5ps09pd0.02h')
+
+ax5=plt.subplot(335)
+ax5.imshow(nlm_5ps_11pd_02h, cmap='Greys_r')
+plt.title('NLM5ps11pd0.02h')
+
+ax6=plt.subplot(336)
+ax6.imshow(nlm_5ps_13pd_02h, cmap='Greys_r')
 plt.title('NLM5ps13pd0.02h')
 
-plt.savefig('plts_nlm_2.tiff',dpi=600) 
+ax7=plt.subplot(337)
+ax7.imshow(nlm_5ps_11pd_02h, cmap='Greys_r')
+plt.title('NLM5ps11pd0.02h')
+
+ax8=plt.subplot(338)
+ax8.imshow(nlm_5ps_11pd_05h, cmap='Greys_r')
+plt.title('NLM5ps11pd0.05h')
+
+ax9=plt.subplot(339)
+ax9.imshow(nlm_5ps_11pd_10h, cmap='Greys_r')
+plt.title('NLM5ps11pd0.10h')
+
+plt.savefig('pltsNLM.tiff',dpi=600) 
 plt.show()
 
+# histogram
+num = (oriClean.shape[0]*oriClean.shape[0])
+listOri50 = ori50.reshape((num,1))
+listNlm05ps11pd02h = nlm_5ps_11pd_02h.reshape((num,1))
+listNlm05ps11pd05h = nlm_5ps_11pd_05h.reshape((num,1))
+listNlm05ps11pd10h = nlm_5ps_11pd_10h.reshape((num,1))
+his_x = np.arange(0.0005,1,0.001)
+
+histOri50 = np.histogram(listOri50, bins=1000, range=(0,1))
+histNlm05ps11pd02h = np.histogram(listNlm05ps11pd02h, bins=1000, range=(0,1))
+histNlm05ps11pd05h = np.histogram(listNlm05ps11pd05h, bins=1000, range=(0,1))
+histNlm05ps11pd10h = np.histogram(listNlm05ps11pd10h, bins=1000, range=(0,1))
+
+ax10 = plt.subplot(111)
+ax10.plot(his_x, histOri50[0])
+ax10.plot(his_x, histNlm05ps11pd02h[0])
+ax10.plot(his_x, histNlm05ps11pd05h[0])
+ax10.plot(his_x, histNlm05ps11pd10h[0])
+plt.legend(['Noisy','histNlm05ps11pd02h','histNlm05ps11pd05h','histNlm05ps11pd10h'])
+plt.xlim((0,1))
+plt.ylim((0,600))
+plt.xticks(np.arange(0,1.25,step=0.25))
+plt.savefig('histNLM.tiff',dpi=600)
+
+# Output files:
+print("###########################")
+print("Files made:")
+print("\t1. histNLM.tiff")
+print("\t2. pltsNLM.tiff")
+print("###########################")
--- readMeFilter.txt
+++ readMeFilter.txt
@@ -1,34 +1,47 @@
-Approach
-  Make particle data using kalipshera
-  Add partial volume effect
-  Add gaussian blurring
-  Add random noise
+Objective:
+  Flter noise from image to have more well defined gl intensity distributions. 
+  Check for fidelity with the original image with PVE only 
+
+Approach:
+  Using Kalisphera: 
+    Get particle data 
+    Add partial volume effect
+    Add gaussian blurring
+    Add random noise
   ---
-  Find effective ways to clean noise
+  Find effective ways to clean noise using different filters
   Check for best performing settings
   Code Filter.py class with the best methods
-
+  The trial filter files are named as alphaFilter.py and betaFilter.py, utill the final class
+    is named as Filter.py 
+  
 Knead to know
+  In literature [1], the main methods listed are [gaussian], [median] and [non-local means]
+    filter to clean the noise in the image. The Gaussian filter does a local weighted
+    averaging of the image over a patch, using gaussian distribution as the weights.
+    This burs out the image - no good.
   The median filter takes a 2D/3D patch over the image and finds median gl of the patch
-    The gl at center of the patch is replaced by the median value
+    The gl at center of the patch is replaced by the median value. This tends to preserve
+    the edge of the particles better (depending on the size of patch chosen). 
   The non-local-means filter exploits self similatity of images [2]
-    The non-local means algorithm replaces the value of a pixel by an average of a selection of 
-    other pixels values: small patches centered on the other pixels are compared to the patch 
-    centered on the pixel of interest, and the average is performed only for pixels that have 
-    patches close to the current patch. As a result, this algorithm can restore well textures, 
-    that would be blurred by other denoising algorithm.
+    The non-local means algorithm replaces the value of a pixel by a weighted average of 
+    a selection of other pixels values: small patches centered on the other 
+    pixels are compared to the patch centered on the pixel of interest, and the 
+    average is performed only for pixels that have patches close to the current 
+    patch. As a result, this algorithm can restore well textures, that would be 
+    blurred by other denoising algorithm.
         
-Issues: 
-  The nlm filters make the gl peaks narrower but shift the peaks for the particles to smaller gl
-  Need to find a way of checking accuracy
+Findings: 
+  1. nlm filters make the gl peaks narrower but shift left the peaks for the 
+  2. Need to find a way of checking accuracy - images look better but needs to
     Different in baseline and non-filtered image 
-      Mean of the difference of image with PVE and after filters is 0.001395 
+    Mean of the difference of image with PVE and after filters is 0.001395 
     
 Conclusions: 
   The non local mean filter does the best job for cleaning the data - the effect of patch
   The  median filter is also good but more noise on the void part
   Median filter does a goood job of identifying the peak locaiton for thye particles
-  The difference between the images - original and after sharpening is shown below and saved.
+  The difference between the images is saved 
 
 References: 
 [1] Vlahinić, Ivan, Edward Andò, Gioacchino Viggiani, and José E. Andrade. 2014. “Towards a More Accurate Characterization of Granular Media: Extracting Quantitative Descriptors from Tomographic Images.” Granular Matter 16 (1): 9–21. https://doi.org/10.1007/s10035-013-0460-6.
@@ -37,3 +50,4 @@ References:
 Links: 
 [1] NLM: https://www.youtube.com/watch?v=9tUns4HYtcw
 [2] NLM math: https://www.youtube.com/watch?v=k8hS6uTz-Uc
+
