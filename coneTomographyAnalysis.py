88ae7827dd80163121b6a94a47d440cebeed75f8
eshanganju <ganju.eshan@gmail.com>
2020-12-09 17:05:03 -0500

    inital outline of the surcharge analysis code. Reader updated.

--- coneTomographyAnalysis.py
+++ coneTomographyAnalysis.py
@@ -4,16 +4,16 @@ Code for the analysis of 3D tomo data for samples collected around the cone pene
 written by @eg
 
 Steps:
-    (01) Initialize the samples variables
-    (02) Read the files and invert if needed
-    (03) Extract subregions from the 3D tomo data - 6D50 edge length
-    (04) Compute particle sizes of the sand samples - user threshold after initial guess from OTSU
-    (05) Get particel aspect ration from PCA length ratios
-    (06) Get particle size distribution
-    (07) Calculate relative breakage parameter
-    (08) Get contact normals
-    (09) Get fabric tensors
-    (10) Get fabric projection plots
+    -Initialize the samples variables
+    -Read the files and extract subregions from the 3D tomo data - 6D50 edge length
+
+    -Compute particle sizes of the sand samples - user threshold after initial guess from OTSU
+    -Get particel aspect ration from PCA length ratios
+    -Get particle size distribution
+    -Calculate relative breakage parameter
+    -Get contact normals
+    -Get fabric tensors
+    -Get fabric projection plots
 
 '''
 
@@ -22,11 +22,25 @@ from pac import Segment
 from pac import Measure
 from pac import Plot
 
-# Each test has three samples - top mid and bottom
-scanData = ['2QR25','2QR25','2QR50','2QR90','OGF25','OGF50','OGF90','OTC25','OTC50','OTC90']
-invertData = True
-imageDataDump = ''
+scanData = ['2QR_25_top','2QR_25_mid','2QR_25_tip',
+            '2QR_50_top','2QR_50_mid','2QR_50_tip',
+            '2QR_90_top','2QR_90_mid','2QR_90_tip',
+            'OGF_25_top','OGF_25_mid','OGF_25_tip',
+            'OGF_50_top','OGF_50_mid','OGF_50_tip',
+            'OGF_90_top','OGF_90_mid','OGF_90_tip',
+            'OTC_25_top','OTC_25_mid','OTC_25_tip',
+            'OTC_50_top','OTC_50_mid','OTC_50_tip',
+            'OTC_90_top','OTC_90_mid','OTC_90_tip']
 
-'''
 
-'''
+for each scan in scanData:
+    scanInputLoc = '/home/eg/codes/pacInput/' + scan + '/'
+    subregionXYList = '/home/eg/codes/pacInput/' + scan + '/' + 'subregionLocs.csv'
+    subregionsXY = np.genfromtxt(subregionXYList, delimiter=',', skiprows=1)
+
+    analysisOutputLoc = '/home/eg/codes/pacOutput/cone/' + scan + '/'
+    imageOutputLoc = '/media/eg/EshanGanju/coneImages/' + scan + '/'
+
+    subregion = 
+
+
--- pac/Reader.py
+++ pac/Reader.py
@@ -4,7 +4,7 @@ This library assists in reading and cropping the CT data files
 
 # General imports
 from skimage.util import img_as_float
-import skimage.external.tifffile as tiffy
+import tifffile as tiffy
 import matplotlib.pyplot as plt
 import numpy as np
 import glob
@@ -23,7 +23,95 @@ def readTiffStack( folderAndFileLocation, cntrZ, cntrY, cntrX, lngt, calib ):
     data = dataBig[ lowerSlice:upperSlice, lowerRow : upperRow, lowerCol : upperCol ]
     return data
 
+def readTiffFileSequence2(folderLocation,centerZ,topLeftY,topLeftX,lngt,calib,invImg=False):
+    """
+    Description:
+        This function reads data from sequence of tiff images from XCT
+        The entire image is not read, instead a cubic subregion specified by the user is extracted from the scan
+
+    Parameters:
+        folderLocation (str) : location of the image sequences
+        centerZ (int) : slice number of center of the subregion
+        topLeftY (int) : Y (vertical) location in pixel units of the lop left corner of the subregion (in correct orientation)
+        topLeftX (int) : X (horizontal) location in pixel units of the lop left corner of the subregion (in correct orientation)
+        lngt (float) : length in mm of the subregion volume
+        calib (float) : calibration factor (mm/pixel)
+        invImg (bool) : Keep true if the image needs to be inverted before extraction
+
+    Return:
+        subregion (np array) : 3D numpy array of extracted subregion.
+    """
+    print( 'Reading tiff files from: ' + folderLocation )
+
+    # Calculating pixel limits
+    upperSlice = centerZ + round( ( lngt / 2 ) / calib)
+    lowerSlice = centerZ - round( ( lngt / 2 ) / calib )
+
+    topRow = topLeftY
+    bottomRow = topLeftY + round(lngt/calib)
+
+    leftCol = topLeftX
+    rightCol = topLeftX + round(lngt/calib)
+
+    print( 'Reading files from: ' + folderLocation )
+    searchString = folderLocation + '/*tif'
+    searchString2 = folderLocation + '/*tiff'
+
+    fileList1 = glob.glob(searchString)
+    fileList2 = glob.glob(searchString2)
+
+    if len(fileList1)>len(fileList2):
+        fileList = fileList1
+        fileList.sort()
+    else:
+        fileList = fileList2
+        fileList.sort()
+
+    numTiffFilesInFolder = len(fileList)
+    numTiffFilesToRead = ( upperSlice - lowerSlice)
+
+    print( 'Number of files in the folder %d' % numTiffFilesInFolder )
+    print( 'Number of files to read %d' % numTiffFilesToRead )
+    print( '\t' + str(lowerSlice) + '-' + str( upperSlice ) )
+
+    tempFile = tiffy.imread( str( fileList[ 0 ] ) )
+    rows = tempFile.shape[ 0 ]
+    columns = tempFile.shape[ 1 ]
+    del tempFile
+
+    gliMap = np.empty( ( numTiffFilesToRead, rows, columns ) )
+
+    # Reading individual files - file number offset
+    for i in range( lowerSlice , upperSlice ):
+        gliMap[ i - lowerSlice ] = tiffy.imread ( fileList [ i ] )
+        print('Read ' + str( i - lowerSlice + 1 ) + '/' + str( numTiffFilesToRead ) + ' files...')
+
+    # Inverting after reading - is this intelligent?
+    if invImg == True: gliMap = invertImage(gliMap)
+    print( '\nFinished reading files...' )
+
+    # Cropping image
+    croppedGLIMap = gliMap[ :, topRow : bottomRow, leftCol : rightCol ]
+    return croppedGLIMap
+
 def readTiffFileSequence( folderLocation, cntrZ, cntrY, cntrX, lngt, calib, invImg=False):
+    """
+    Description:
+        This function reads data from sequence of tiff images from XCT
+        The entire image is not read, instead a cubic subregion specified by the user is extracted from the scan
+
+    Parameters:
+        folderLocation (str) : location of the image sequences
+        cntrZ (int) : slice number of center of the subregion
+        cntrY (int) : Y (vertical) location in pixel units of the center of the subregion (in correct orientation)
+        cntrX (int) : X (horizontal) location in pixel units of the center of the subregion (in correct orientation)
+        lngt (float) : length in mm of the subregion volume
+        calib (float) : calibration factor (mm/pixel)
+        invImg (bool) : Keep true if the image needs to be inverted before extraction
+
+    Return:
+        subregion (np array) : 3D numpy array of extracted subregion.
+    """
     print( 'Reading tiff files from: ' + folderLocation )
 
     upperSlice = cntrZ + round( ( lngt / 2 ) / calib )
