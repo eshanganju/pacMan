2f51d0c6c5e1990feb46ddca13197b91dd1f650d
eshanganju <ganju.eshan@gmail.com>
2021-02-20 18:29:25 -0500

    adding updated scripts for cone tomography analysis and radius ratio check

--- coneTomographyAnalysis.py
+++ coneTomographyAnalysis.py
@@ -16,61 +16,92 @@ Steps:
 
 '''
 
-from pac import Reader
+from pac import Read
 from pac import Filter
 from pac import Segment
 from pac import Measure
 from pac import Plot
 
 v=int(input('Enter number: '))
+userH=int(1)
+userRR=0.6
+mainInput = '/home/eg/codes/pacInput/'
+mainOutput = '/home/eg/codes/pacOutput/cone/finalFolder/'
 
 # 2QR 25
 if v==1:
     scanList = ['2QR_25_tip','2QR_25_mid','2QR_25_top']
     originalPSDLoc = '/home/eg/codes/pacInput/originalGSD/2qrOrig.csv'
-    psdOrig = Reader.readDataFromCsv( originalPSDLoc,
-                                      maxRows=10,
-                                      dataForm='array').reshape(10,2)
+    psdOrig = Read.readDataFromCsv( originalPSDLoc,
+                                    maxRows=10,
+                                    dataForm='array').reshape(10,2)
 # 2QR 50
-#if v==2:
+if v==2:
+    scanList = ['2QR_50_tip','2QR_50_mid','2QR_50_top']
+    originalPSDLoc = '/home/eg/codes/pacInput/originalGSD/2qrOrig.csv'
+    psdOrig = Read.readDataFromCsv( originalPSDLoc,
+                                    maxRows=10,
+                                    dataForm='array').reshape(10,2)
 
 # 2QR 90
-#if v==3:
+if v==3:
+    scanList = ['2QR_90_tip','2QR_90_mid','2QR_90_top']
+    originalPSDLoc = '/home/eg/codes/pacInput/originalGSD/2qrOrig.csv'
+    psdOrig = Read.readDataFromCsv( originalPSDLoc,
+                                    maxRows=10,
+                                    dataForm='array').reshape(10,2)
 
 # OGF 25
 if v==4:
     scanList = ['OGF_25_tip','OGF_25_mid','OGF_25_top']
     originalPSDLoc = '/home/eg/codes/pacInput/originalGSD/ogfOrig.csv'
-    psdOrig = Reader.readDataFromCsv( originalPSDLoc,
-                                      maxRows=10,
-                                      dataForm='array').reshape(10,2)
+    psdOrig = Read.readDataFromCsv( originalPSDLoc,
+                                    maxRows=10,
+                                    dataForm='array').reshape(10,2)
 
 # OGF 50
-#if v==5:
+if v==5:
+    scanList = ['OGF_50_tip','OGF_50_mid','OGF_50_top']
+    originalPSDLoc = '/home/eg/codes/pacInput/originalGSD/ogfOrig.csv'
+    psdOrig = Read.readDataFromCsv( originalPSDLoc,
+                                    maxRows=10,
+                                    dataForm='array').reshape(10,2)
 
 # OGF 90
-#if v==6:
+if v==6:
+    scanList = ['OGF_90_tip','OGF_90_mid','OGF_90_top']
+    originalPSDLoc = '/home/eg/codes/pacInput/originalGSD/ogfOrig.csv'
+    psdOrig = Read.readDataFromCsv( originalPSDLoc,
+                                    maxRows=10,
+                                    dataForm='array').reshape(10,2)
 
 # OTC 25
 if v==7:
     scanList = ['OTC_25_tip','OTC_25_mid','OTC_25_top']
     originalPSDLoc = '/home/eg/codes/pacInput/originalGSD/otcOrig.csv'
-    psdOrig = Reader.readDataFromCsv( originalPSDLoc,
-                                      maxRows=10,
-                                      dataForm='array').reshape(10,2)
+    psdOrig = Read.readDataFromCsv( originalPSDLoc,
+                                    maxRows=10,
+                                    dataForm='array').reshape(10,2)
 # OTC 50
-#if v==8:
+if v==8:
+    scanList = ['OTC_50_tip','OTC_50_mid','OTC_50_top']
+    originalPSDLoc = '/home/eg/codes/pacInput/originalGSD/otcOrig.csv'
+    psdOrig = Read.readDataFromCsv( originalPSDLoc,
+                                    maxRows=10,
+                                    dataForm='array').reshape(10,2)
 
 # OTC 90
-#if v==9:
+if v==9:
+    scanList = ['OTC_90_tip','OTC_90_mid','OTC_90_top']
+    originalPSDLoc = '/home/eg/codes/pacInput/originalGSD/otcOrig.csv'
+    psdOrig = Read.readDataFromCsv( originalPSDLoc,
+                                    maxRows=10,
+                                    dataForm='array').reshape(10,2)
 
 numberofSubregionsPerScan=10
 nD50=6
 
 for scan in scanList:
-    mainInput = '/home/eg/codes/pacInput/'
-    mainOutput = '/home/eg/codes/pacOutput/cone/testEntire/'
-
 
     # Locations of data
     scanInputLoc = mainInput + scan + '/'
@@ -78,28 +109,28 @@ for scan in scanList:
     outputLoc = mainOutput + scan + '/'
 
     # Reading subregion data saved in the subregionInfo location.
-    subregionCalib = Reader.readDataFromCsv( subregionInfo,
+    subregionCalib = Read.readDataFromCsv( subregionInfo,
                                              skipHeader=1,
                                              maxRows=1,
                                              fmt='float',
                                              dataForm='number' )
 
     # The average particle size of the sand
-    subregionD50 = Reader.readDataFromCsv( subregionInfo,
+    subregionD50 = Read.readDataFromCsv( subregionInfo,
                                            skipHeader=2,
                                            maxRows=1,
                                            fmt='float',
                                            dataForm='number')
 
     # Slice number for center of the scan
-    subregionZ = Reader.readDataFromCsv( subregionInfo,
+    subregionZ = Read.readDataFromCsv( subregionInfo,
                                          skipHeader=3,
                                          maxRows=1,
                                          fmt='int',
                                          dataForm='number')
 
     # Location of points in the scan - top left corner of the cube
-    subregionYXArray = Reader.readDataFromCsv( subregionInfo,
+    subregionYXArray = Read.readDataFromCsv( subregionInfo,
                                                skipHeader=4,
                                                maxRows=numberofSubregionsPerScan,
                                                fmt='int',
@@ -110,7 +141,7 @@ for scan in scanList:
         currentSampleName = scan + '-' + str( round( currentSubregion ) )
 
         # Extraction of the subregion from the complete scan
-        subregionGLIMap = Reader.extractSubregionFromTiffSequence( folderDir=scanInputLoc,
+        subregionGLIMap = Read.extractSubregionFromTiffSequence( folderDir=scanInputLoc,
                                                                    reference='topLeft',
                                                                    Z=subregionZ,
                                                                    Y=subregionYXArray[currentSubregion,0],
@@ -137,7 +168,7 @@ for scan in scanList:
                                                   outputDir=outputLoc )
         # EDM peaks
         edmPeaksMap = Segment.obtainLocalMaximaMarkers( edMapForPeaks=edmMap,
-                                                        h=int(1),
+                                                        h=userH,
                                                         sampleName=currentSampleName,
                                                         saveImg=False,
                                                         outputDir=outputLoc )
@@ -155,9 +186,9 @@ for scan in scanList:
                                                      pad=int(2),
                                                      areaLimit=700,
                                                      considerEdgeLabels=True,
-                                                     checkForSmallParticles=True,
+                                                     checkForSmallParticles=False,
                                                      radiusCheck=True,
-                                                     radiusRatioLimit=0.6,
+                                                     radiusRatioLimit=userRR,
                                                      sampleName=currentSampleName,
                                                      saveImg=True,
                                                      outputDir=outputLoc )
