eb47ed28bba63858389025f1adc8e98c989f7eb3
eshanganju <ganju.eshan@gmail.com>
2022-08-13 16:38:42 -0400

    inital commit for AM analysis

--- aiml2.py
+++ aiml2.py
@@ -1,3 +1,17 @@
-"""Noise in binarized
+"""
+Pipeline to segment pores a and solid phase
 """
 
+from pac import Segment
+from pac import Measure
+import tifffile as tf
+
+# Read 
+
+# Parameters
+
+# Segmentation
+
+# Size analysis
+
+# Cleanup - Adjacent occurrence of small pores and particles
